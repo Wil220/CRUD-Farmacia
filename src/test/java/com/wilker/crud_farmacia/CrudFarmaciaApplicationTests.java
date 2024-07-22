80f31bb62e615b5386e3c509512b0ffd89616b05
Wil220 <wilker.silva0714@gmail.com>
2024-07-22 10:03:21 -0300

    primeiro commit

---
+++ src/test/java/com/wilker/crud_farmacia/CrudFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilker.crud_farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
